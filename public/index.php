240cb87e5ee4274f58ef0726784bffd6fca0a7f9
Jonh Rodrigues <jonhlennon8@gmail.com>
2014-01-22 19:20:25 -0200

    Primeiro Commit

---
+++ public/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE HTML5>
+<HTML LANG="pt-BR">
+	<HEAD>
+		<META CHARSET="utf-8">
+		<TITLE>Pesquisa</TITLE>
+		<LINK REL="shortcut icon" TYPE="image/x-icon" HREF="favicon.ico" />
+		<!--<LINK REL="stylesheet" TYPE="text/css" HREF="geral.css" />-->
+	</HEAD>
+	<BODY>
+		<FORM ID="busca" ACTION="http://www.google.com.br/search" METHOD="get">
+		<FIELDSET>
+			<INPUT TYPE="search" name="q" REQUIRED AUTOFOCUS />
+			<INPUT TYPE="submit" value="buscar" />
+			<INPUT TYPE="reset" value="limpar" />
+		</FIELDSET>
+		</FORM>
+	</BODY>
+</HTML>
